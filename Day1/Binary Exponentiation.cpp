27521d70d9b7e0195e28e066f74119f0b3853886
Nishant Handge <60032250+Nishant127@users.noreply.github.com>
2020-10-05 23:32:36 +0530

    Create Binary Exponentiation.cpp

---
+++ Day1/Binary Exponentiation.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int expo(int n,int a)
+{
+    int res=1;
+    while(a)
+    {
+        if(a%2)
+        res*=n, a--;
+        else
+        n*=n,  a/=a;
+    }
+    return res;
+}
+int main()
+{
+    int n,t,a;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>a;
+        cout<<expo(n,a)<<endl;
+    }
+}
