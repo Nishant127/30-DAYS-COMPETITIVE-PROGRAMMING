b8074d02cde9b321241ed728c649c8adb5ffc7e7
Nishant Handge <60032250+Nishant127@users.noreply.github.com>
2020-10-31 23:20:37 +0530

    Add files via upload

---
+++ DAY27/Array/Two Sum Problem (Leetcode).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        vector<int>ans;
+        int i,j;
+        for(i=0;i<nums.size();i++)
+        {
+            if(mp.find(target-nums[i])!=mp.end())
+            {
+                ans.push_back(mp[target-nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            mp[nums[i]]=i;
+        }
+        return ans;
+    }
+};
